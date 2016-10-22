20db2e257beca39cb96a6986455a29f0399844e0
Hyojun Kang <admire9@gmail.com>
2016-10-22 18:07:16 +0900

    Install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 NVIM_DIR="$HOME/.config/nvim"
-ln -s "`pwd`/nvim/init.vim" "$NVIM_DIR/init.vim"
+ln -s "$(pwd)/nvim/init.vim" "$NVIM_DIR/init.vim"
 
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+nvim +PlugInstall +q
