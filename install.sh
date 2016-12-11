d5590cb2ec5e0b87a1ca799583f78f9cd815bff1
Hyojun Kang <admire9@gmail.com>
2016-12-11 11:36:01 +0900

    Update and quit vim

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-nvim +PlugInstall +q
+nvim +PlugInstall +PlugUpdate +qa
